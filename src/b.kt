4a92ca1f9eaf23614205eb9fbab387259d28a490
nevikw39 <nevikw39@icloud.com>
2024-05-09 10:57:18 +0800

    B - Compare T-Shirt Sizes

---
+++ src/b.kt
@@ -0,0 +1,20 @@
+private fun solve() {
+    val (a, b) = readln().split(' ').map {
+        when (it.last()) {
+            'S' -> -it.length
+            'L' -> it.length
+            else -> 0
+        }
+    }
+    println(
+        when (a.compareTo(b)) {
+            1 -> '>'
+            0 -> '='
+            else -> '<'
+        }
+    )
+}
+
+fun main() {
+    repeat(readln().toInt()) { solve() }
+}
