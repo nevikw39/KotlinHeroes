f675ec5f09a8ceb335ce916ef14af7e032cd7d93
nevikw39 <nevikw39@icloud.com>
2024-05-10 02:54:54 +0800

    E - Jumping on Tiles

---
+++ src/e.kt
@@ -0,0 +1,16 @@
+import kotlin.math.min
+import kotlin.math.max
+
+private fun solve() {
+    val s = readln()
+    val a = min(s.first().code, s.last().code)
+    val b = max(s.first().code, s.last().code)
+    val r = s.indices.filter { s[it].code in a..b }
+        .sortedWith(if (s.first().code < s.last().code) compareBy<Int> { s[it] }.thenBy { it } else compareByDescending<Int> { s[it] }.thenBy { it })
+    println("${b - a} ${r.size}")
+    println(r.map { it + 1 }.joinToString(" "))
+}
+
+fun main() {
+    repeat(readln().toInt()) { solve() }
+}
